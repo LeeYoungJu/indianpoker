a444c37ab6b5e418fe4c4147ad094ec85263c455
LeeYoungJu <dmlqhqhqh@naver.com>
2014-01-04 19:08:59 +0900

    remove unuseful file

--- readfile.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-
-fs.watchFile('./test.txt', {persistent: true, interval: 0}, function(curr, prev) {
-	console.log('current time' + curr.mtime);
-	console.log('previous time' + prev.mtime);
-});
